b7a7cc396c50f11630bffc02b9c0ceb164c8024b
szymon <szymek.januszek@gmail.com>
2019-12-27 13:21:00 +0100

    Finally resolved server issues. Now in order to work, mod is required on both sides.

--- src/main/java/net/szum123321/elytra_swap/ElytraSwap.java
+++ src/main/java/net/szum123321/elytra_swap/ElytraSwap.java
@@ -1,14 +1,17 @@
 package net.szum123321.elytra_swap;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class ElytraSwap implements ModInitializer {
-    public final String MOD_ID = "elytra_swap";
+    public static final String MOD_ID = "elytra_swap";
+    public static final Identifier KICK_PLAYER_INTO_AIR = new Identifier(MOD_ID, "kick");
 
     @Override
     public void onInitialize() {
         System.out.println("Loading Elytra Swap by Szum123321");
+
         TakeOffHandler.onItemUseRegister();
     }
-
 }
+
