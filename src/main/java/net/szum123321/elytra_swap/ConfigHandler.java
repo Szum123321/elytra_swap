c87fa92b76eb506aa31e53ed686cdb4d015b9b40
szymon <szymek.januszek@gmail.com>
2019-12-30 09:17:50 +0100

    Resolved even more issues. Mod now uses cotton-config as its config manager, which comes as jar-in-jar. Added keybing for enabling/disabling elytra swap. And many more tweaks

--- src/main/java/net/szum123321/elytra_swap/ConfigHandler.java
+++ src/main/java/net/szum123321/elytra_swap/ConfigHandler.java
@@ -7,7 +7,7 @@ import io.github.cottonmc.cotton.config.annotations.ConfigFile;
 public class ConfigHandler {
     @Comment(value = "Changes how server treats players without mod installed\n" +
             "0 (default): players without Elytra Swap installed are just ignored\n"+
-            "1: players without Elytra Swap installed will be teleported into air and armour change will be forced\n"+
+            "1: players without Elytra Swap installed will be teleported into air and armor change will be forced\n"+
             "2: for players without Elytra Swap installed armour still will be changed when falling but firework usage will be unavailable"
     )
     public int noModPlayersHandlingMethod = 0;
